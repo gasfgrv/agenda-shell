8211c2460059ea2e10e1ed6b2e94320af726b745
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-12-26 12:59:36 -0300

    correções e documentação

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# Agenda Shell
+# Agenda Shell<!-- omit from toc -->
 
 Script que serve como uma agenda telefonica que salva e consulta os dados em um arquivo txt.
+
+## Tabela de conteúdos <!-- omit from toc -->
+
+- [Features](#features)
+- [Como rodar](#como-rodar)
+  - [Salvar Contato](#salvar-contato)
+  - [Buscar contato](#buscar-contato)
+  - [Sair](#sair)
+
+## Features
+
+- [x] Cadastro de contatos
+- [x] Pesquisa de contatos
+
+## Como rodar
+
+Rodar o script `path/to/agenda-shell/agenda.sh`
+
+Irá abrir o menu inicial:
+
+![docs/img.png](docs/img.png)
+
+### Salvar Contato
+
+Escolher a opção 1, que abrirá o prompt para colocar os dados
+
+![docs/img_1.png](docs/img_1.png)
+
+### Buscar Contato
+
+Escolher a opção 2, que abrirá um sub menu para escolher o atributo da busca:
+
+![docs/img_2.png](docs/img_2.png)
+
+Após escolher, basta passar a informação para o prompt
+
+![docs/img_3.png](docs/img_3.png)
+
+![docs/img_4.png](docs/img_4.png)
+
+### Sair
+
+Escolher a opção 3 e confirmar
+
+![docs/img_5.png](docs/img_5.png)
--- agenda.sh
+++ agenda.sh
@@ -1,66 +1,60 @@
 #!/bin/bash
-while :
-do
+while :; do
+  clear
+  echo "Menu:"
+  echo "1) Adicionar contato"
+  echo "2) Pesquisar"
+  echo "3) Sair"
+  read -r -p "Opção desejada: " opc
+  case $opc in
+  1)
     clear
-    echo "Menu:"
     echo "1) Adicionar contato"
+    read -r -p "Nome: " nome
+    read -r -p "Telefone: " tel
+    echo "$nome - $tel" >>./AgendaTelefonica.txt
+    echo "Contato adicionado !"
+    sleep 1
+    clear
+    ;;
+  2)
+    clear
     echo "2) Pesquisar"
-    echo "3) Sair"
-    echo ""
-    echo "Opção desejada: "
-    read opc
-    case $opc in
-        1)
-            clear
-            echo "1) Adicionar contato"
-            echo "Nome: "
-            read nome
-            echo "Telefone: "
-            read tel
-            echo $nome " - " $tel >> AgendaTelefonica.txt
-            echo "Contato adicionado !"
-            sleep 1
-            clear
-        ;;
-        2) clear
-            echo "2) Pesquisar"
-            echo "Pesquisar por: "
-            echo "1) Nome"
-            echo "2) Telefone"
-            read opcPesq
-            case $opcPesq in
-                1)
-                    echo "Digite o nome: "
-                    read nome
-                    clear
-                    grep -i $nome AgendaTelefonica.txt
-                    sleep 5
-                    clear
-                ;;
-                2)
-                    echo "Digite o Telefone: "
-                    clear
-                    read tel
-                    grep -i $tel AgendaTelefonica.txt
-                    sleep 5
-                    clear
-                ;;
-                *)
-                    echo "Opção Inválida, digite novamente"
-                ;;
-            esac
-        ;;
-        *) clear
-            echo "Desejar sair? (s/n)"
-            read resp
-            if [ $resp == "s" ]
-            then
-                clear
-                echo "Saindo..."
-                sleep 1
-                clear
-                exit 0
-            fi
-        ;;
+    echo "1) Nome"
+    echo "2) Telefone"
+    read -r -p "Pesquisar por: " opcPesq
+    case $opcPesq in
+    1)
+      clear
+      read -r -p "Digite o nome: " nome
+      clear
+      grep -i "$nome" AgendaTelefonica.txt
+      sleep 5
+      clear
+      ;;
+    2)
+      clear
+      read -r -p "Digite o Telefone: " tel
+      clear
+      grep -i "$tel" AgendaTelefonica.txt
+      sleep 5
+      clear
+      ;;
+    *)
+      echo "Opção Inválida, digite novamente"
+      ;;
     esac
+    ;;
+  *)
+    clear
+    read -r -p "Desejar sair? (s/n) " resp
+    if [ "$resp" == "s" ]; then
+      clear
+      echo "Saindo..."
+      sleep 1
+      clear
+      exit 0
+    fi
+    ;;
+  esac
 done
